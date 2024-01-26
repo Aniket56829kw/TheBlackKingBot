0c9a86d3e951f7cc422dc6d1b6a8f90136f4fbb0
TheBlackXYZ24 <155623566+TheBlackxyz@users.noreply.github.com>
2024-01-26 15:24:45 +0530

    Add files via upload

---
+++ server/__init__.py
@@ -0,0 +1,2 @@
+# Dont mind - Main aisa hi hun - @TheBlackXYZ
+# I hate copy pasters - Give Proper credit.
---
+++ server/exceptions.py
@@ -0,0 +1,12 @@
+    # Credit@TheBlackXYZ.
+# Please Don't remove credit.
+# Born to make history @TheBlackXYZ !
+# Thank you LazyDeveloper for helping us in this Journey
+# 🥰  Thank you for giving me credit @TheBlackXYZ  🥰
+# for any error please contact me -> telegram@TheBlackXYZ or insta @TheBlackXYZ
+# rip paid developers 🤣 - >> No need to buy paid source code while @TheBlackXYZ is here 😍😍 
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
