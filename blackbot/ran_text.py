926116eadb9cca686f8b6e12cdbcf1105f54a9c1
TheBlackXYZ24 <155623566+TheBlackxyz@users.noreply.github.com>
2024-01-26 15:18:43 +0530

    Add files via upload

---
+++ blackbot/ran_text.py
@@ -0,0 +1,10 @@
+# with Love @TheBlackXYZ 💘
+# Subscribe YT @TheBlackXYZ - to learn more about this for free...
+
+import random
+import string
+
+def random_char(y):
+       return ''.join(random.choice(string.ascii_letters) for x in range(y))
+
+ran = (random_char(5))
